160a4988020244aaf3b70a87daed4769cdccbbba
Hyungmo Gu <guhyungm7@gmail.com>
2020-12-31 09:40:23 +0900

    Add box component

--- main/src/App.js
+++ main/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
+// function Draggable() {
+//     return (
+//         <div>
+
+//         </div>
+//     );
+// }
+
+function Box() {
+    return (
+        <div className="box"></div>
+    );
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Box/>
+    );
 }
 
 export default App;
