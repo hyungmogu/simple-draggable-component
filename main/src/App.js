3fcd56c897edbf5cd5977477f80dde438194674d
Hyungmo Gu <guhyungm7@gmail.com>
2020-12-31 12:49:47 +0900

    Make Box not go out of Draggable component

--- main/src/App.js
+++ main/src/App.js
@@ -1,81 +1,26 @@
 import './App.css';
 import React from  'react';
 
-class Draggable extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        return (
-            <div className="draggable-area">
-                {this.props.children}
-            </div>
-        );
-    }
-}
-
-class Box extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            dragStart: [0,0],
-            style: {
-                left: '0px',
-                top: '0px'
-            }
-        }
-    }
-
-    handleDragStart = (e) => {
-        // Store the position of mouse drag to state variable 'dragStart' when drag starts
-
-        this.setState((state, props) => {
-            return {
-                start: [e.clientX, e.clientY]
-            }
-        });
-    }
-
-    handleDragEnd = (e) => {
-        // On dropping the box, find the position of mouse drop (clientX, clientY)
-        // Find change in box's position
-        let diffX = e.clientX - this.state.start[0];
-        let diffY = e.clientY - this.state.start[1];
-
-        // Calculate box's new position
-        // Apply new left position
-        // Apply new top position
-        this.setState((state, props) => {
-            let newLeft = `${parseFloat(state.style.left) + diffX}px`;
-            let newTop = `${parseFloat(state.style.top) + diffY}px`;
-
-            return {
-                style: {
-                    left: newLeft,
-                    top: newTop
-                }
-            }
-        });
-    }
-
-    render() {
-        return (
-            <div style={this.state.style} className="box" draggable="true" onDragStart={this.handleDragStart} onDragEnd={this.handleDragEnd}></div>
-        );
-    }
-}
+import { AppProvider } from './components/Context';
+import Draggable from './components/Draggable';
+import Box from './components/Box';
 
 function App() {
     return (
-        <main>
-            <Draggable>
-                <Box/>
-                <Box/>
-                <Box/>
-            </Draggable>
-        </main>
+        <AppProvider value={{
+            boardWidth: '500px',
+            boardHeight: '500px',
+            boxWidth: '100px',
+            boxHeight: '100px'}}
+        >
+            <main>
+                <Draggable>
+                    <Box/>
+                    <Box/>
+                    <Box/>
+                </Draggable>
+            </main>
+        </AppProvider>
     );
 }
 
